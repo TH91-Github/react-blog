430215bdbf6562164417f3aa1c5e3be09adc7925
김태훈 <je64768035@gmail.com>
2024-04-15 15:02:43 +0900

    git test

---
+++ src/pages/user/JoinPage.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export default function JoinPage(){
+  return (
+    <StyleJoin>
+      JoinPage
+    </StyleJoin>
+  )
+}
+
+const StyleJoin = styled.div`
+
+`;
---
+++ src/pages/user/Login.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export default function Login(){
+  return (
+    <StyleLogin>
+      JoinPage
+    </StyleLogin>
+  )
+}
+
+const StyleLogin = styled.div`
+
+`;
