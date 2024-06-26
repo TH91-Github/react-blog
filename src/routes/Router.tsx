4e757ee39250f8f8de207f6e21ad4c402b93307e
김태훈 <je64768035@gmail.com>
2024-06-26 10:20:28 +0900

    test

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,7 +3,7 @@ import {createBrowserRouter} from 'react-router-dom';
 import App from 'App';
 import Error from 'components/layout/Error';
 import { routerList } from './RouterList';
-
+ 
 export const router = createBrowserRouter([
   {
     path: "/",
