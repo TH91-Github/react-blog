dc5b35936c857df639c98f74e0e08034655f5db5
김태훈 <je64768035@gmail.com>
2024-06-27 16:21:10 +0900

    Fix : logo 위치 조정

--- src/components/element/Logo.tsx
+++ src/components/element/Logo.tsx
@@ -36,19 +36,19 @@ const StyleLogo = styled.div`
       position:relative;
       width:30px;
       height:30px;
-      gap:2px;
+      gap:1px;
       // pointer-events: none;
       & > span{
         display:block;
-        width:calc((100% - 4px) / 3);
-        height:calc((100% - 4px) / 3);
+        width:calc((100% - 2px) / 3);
+        height:calc((100% - 2px) / 3);
         border-radius:2px;
         background:${colors.originWhite};
         box-shadow: rgba(127, 127, 127, 0.4) 0.5px 0.5px 1px;
         transition: all .3s;
         animation-duration: 7s;
         animation-iteration-count: infinite;
-        animation-fill-mode: both;
+        animation-fill-mode: none;
         &:hover { 
           background:${colors.yellow};  
         }
@@ -56,7 +56,7 @@ const StyleLogo = styled.div`
           animation-name: cube1;
           @keyframes cube1 {
             1.19%{background: ${colors.blue};}
-            2.38%{background:${colors.originWhite};}
+            2.38%, 100%{background:${colors.originWhite};}
           }
         }
         &:nth-child(2){
@@ -64,14 +64,14 @@ const StyleLogo = styled.div`
           @keyframes cube2 {
             2.38%, 4.76% {background:${colors.originWhite};}
             3.57% {background: ${colors.blue};}
-            64.848%, 93.412% {transform:translateY(0)}
-            71.99%, 86.27% {transform:translateY(100%)}
+            64.848%, 93.412%, 100% {transform:translateY(0)}
+            71.99%, 86.27% {transform:translateY(calc(100% + 1px))}
           }
         }
         &:nth-child(3){
           animation-name: cube3;
           @keyframes cube3 {
-            4.76%, 7.14% {background:${colors.originWhite};}
+            4.76%, 7.14%, 100% {background:${colors.originWhite};}
             5.95% {background: ${colors.blue};}
           }
         }
@@ -80,14 +80,14 @@ const StyleLogo = styled.div`
           @keyframes cube4 {
             7.14%, 9.52% {background:${colors.originWhite};}
             8.33% {background: ${colors.blue};}
-            29.142%, 57.706%{transform:translateX(0)}
-            36.284%, 50.564% {transform:translateX(100%)}
+            29.142%, 57.706%, 100%{transform:translateX(0)}
+            36.284%, 50.564% {transform:translateX(calc(100% + 1px))}
           }
         }
         &:nth-child(5){
           animation-name: cube5;
           @keyframes cube5 {
-            9.52%, 11.9% {background:${colors.originWhite};}
+            9.52%, 11.9%, 100% {background:${colors.originWhite};}
             10.71% {background: ${colors.blue};}
           }
         }
@@ -96,8 +96,8 @@ const StyleLogo = styled.div`
           @keyframes cube6 {
             11.9%, 14.28% {background:${colors.originWhite};}
             13.09% {background: ${colors.blue};}
-            29.142%, 57.706%{transform:translateX(0)}
-            36.284%, 50.564% {transform:translateX(-100%)}
+            29.142%, 57.706%, 100%{transform:translateX(0)}
+            36.284%, 50.564% {transform:translateX(calc((100% + 1px) * -1))}
           }
         }
         &:nth-child(7){
@@ -105,8 +105,8 @@ const StyleLogo = styled.div`
           @keyframes cube7 {
             14.28%, 16.66% {background:${colors.originWhite};}
             15.47% {background: ${colors.blue};}
-            29.142%, 57.706%{transform:translateX(0)}
-            36.284%, 50.564% {transform:translateX(100%)}
+            29.142%, 57.706%, 100%{transform:translateX(0)}
+            36.284%, 50.564% {transform:translateX(calc(100% + 1px))}
           }
         }
         &:nth-child(8){
@@ -114,8 +114,8 @@ const StyleLogo = styled.div`
           @keyframes cube8 {
             16.66%, 19.04% {background:${colors.originWhite};}
             17.85% {background: ${colors.blue};}
-            64.848%, 93.412% {transform:translateY(0)}
-            71.99%, 86.27% {transform:translateY(-100%)}
+            64.848%, 93.412%, 100% {transform:translateY(0)}
+            71.99%, 86.27% {transform:translateY(calc((100% + 1px) * -1))}
           }
         }
         &:nth-child(9){
@@ -123,8 +123,8 @@ const StyleLogo = styled.div`
           @keyframes cube9 {
             19.04%, 21.42% {background:${colors.originWhite};}
             20.23% {background: ${colors.blue};}
-            29.142%, 57.706%{transform:translateX(0)}
-            36.284%, 50.564% {transform:translateX(-100%)}
+            29.142%, 57.706%, 100%{transform:translateX(0)}
+            36.284%, 50.564% {transform:translateX(calc((100% + 1px) * -1))}
           }
         }
 
